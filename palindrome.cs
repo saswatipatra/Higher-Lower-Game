9d567c166a2315a7b265921f18f0bb2f032c2795
Jessica Munoz and Saswati Patra <saswati@>
2019-07-16 13:33:06 -0700

    add array.reverse function

--- palindrome.cs
+++ palindrome.cs
@@ -4,18 +4,10 @@ class Palindrome
 {
     static void Main()
     {
-    Console.WriteLine("Enter first side of the triangle ");
+    Console.WriteLine("Enter the word to check whether it's a palindorme or not: ");
     string userString = Console.ReadLine();
-    char[] charArr = userString.ToCharArray();
-    for(int i=0; i>charArr.Length; i--)
-    {
-    for(int  j=charArr.Length;  j=0 ; j--)
-    {
-      Array.Reverse (charArr,i , j);
-    }
-    }
-   string reverseString= string.Join(",", charArr);
-    if (userString==reverseString)
+    bool status=getStatus(userString);
+    if (status== true)
     {
       Console.WriteLine("it's a palindorme");
 
@@ -24,15 +16,13 @@ class Palindrome
     {
       Console.WriteLine("Not a palindorme");
     }
-
-
     }
-    // static bool checkPal(string userString)
-    // {
-    //   char[] charArr = userString.ToCharArray();
-    //   for (int i = charArr.Length; i > 0; i--)
-    //   {
-    //     string reverse
-    //   }
-    // }
+    public static bool getStatus(string userString)
+    {
+    char[] charArr = userString.ToCharArray();
+    Array.Reverse (charArr);
+    string reverseString= new string(charArr);
+    return userString.Equals(reverseString);
+    }
+
 }
